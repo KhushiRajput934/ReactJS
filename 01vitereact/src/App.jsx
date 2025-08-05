148074fe2aa3f444f3258d06ade5717558aa542e
Khushi <kspkhushi.143@gmail.com>
2025-08-05 20:26:23 +0530

    second lecture completed

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -3,9 +3,11 @@ import First from "./first"
 
 
 function App() {
+  const username = "Khushi Rajput"
+// {} currly brase syntax jha bho ye hoga use variable ki trh use kre ge , {username} -> ye hai evaluated expresion yaha sirf final outcome likh skte hai 
   return (
     <>
-      <h1>Welcome to my first react project</h1>
+      <h1>Hii! I am {username} Welcome to my first react project</h1>
       <First />
     </>
   )
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-
 import App from './App.jsx'
+import React from 'react'
+
+// const ReactElement = React.createElement(
+//   'a', //tagname
+//   {herf: 'https://google.com', target: '_blank'}, //object -> in which the props are declared 
+//   'Click me to visit google' // text
+// )
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+<App />
 )
